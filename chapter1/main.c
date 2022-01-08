6d98840fa965852d82579185256b6b770bebac58
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-08 11:12:56 +0530

    celsius to fahrenheit with character and float right aligned

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 
 /*
-fahrenheit to celsius converter
+celsius to fahrenheit converter
 */
 int main(int argc, char **argv)
 {
 	float fahr, celsius;
 	int lower, upper, step;
-	lower = 0;
-	upper = 300;
+	lower = -100;
+	upper = 100;
 	step = 20;
-	fahr = lower;
-	printf("Fahr\tCelsius\n");
-	while(fahr <= upper){
-		celsius = 5 * (fahr - 32) / 9;
-		printf("%3.0f\t%6.1f\n", fahr, celsius);
-		fahr += step;
+	celsius = lower;
+	printf("%3c\t%6c\n", 'C', 'F');
+	while(celsius <= upper){
+		fahr = (9.0 / 5.0) * celsius;
+		fahr += 32;
+		printf("%3.0f\t%6.1f\n", celsius, fahr);
+		celsius += step;
 	}
 	return 0;
 }
