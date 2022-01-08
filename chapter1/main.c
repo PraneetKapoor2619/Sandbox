6f7375a5c4c80e0aa324d7f4edef06b1f9ecb43d
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-08 10:44:55 +0530

    Integer division truncates to zero, fractional parts discarded

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
+/*
+fahrenheit to celsius converter
+*/
 int main(int argc, char **argv)
 {
-	printf("hello, world\n");
+	int fahr, celsius;
+	int lower, upper, step;
+	lower = 0;
+	upper = 300;
+	step = 20;
+	fahr = lower;
+	while(fahr <= upper){
+		celsius = (5 / 9)* (fahr - 32);
+		printf("%d\t%d\n", fahr, celsius);
+		fahr += step;
+	}
 	return 0;
 }
