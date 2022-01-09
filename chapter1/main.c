b884ef86b34b196145879d9fc0d90d67d488c4b2
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-09 11:53:52 +0530

    count characters in input, second version

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
-/*count characters in input, 1st version*/
+/*count characters in input, 2nd version*/
 int main(int argc, char **argv)
 {
-	long nc;
-	nc = 0;
-	while(getchar() != EOF)		/*Press Ctrl + D to reach EOF*/
-		++nc;
-	printf("%ld\n", nc);
+	double nc;
+	
+	for(nc = 0; getchar() != EOF; ++nc)
+		;
+	
+	printf("%.0f\n", nc);
 	return 0;
 }
