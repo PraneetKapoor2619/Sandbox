43a4a672f8fbba87f23a15bb508f651a56285650
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-09 12:39:48 +0530

    print input one word per line

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,33 +1,17 @@
 #include<stdio.h>
 
-#define IN 1	/*inside a word*/
-#define OUT 0	/*inside a word*/
-
-/*count lines, words, and characters in input*/
+/* print input one word per line*/
 int main(int argc, char **argv)
 {
-	int c, nl, nw, nc, state;
-	
-	state = OUT;
-	nl = nw = nc = 0;
+	int c;
 	while((c = getchar()) != EOF){
-		++nc;
 		if(((c >= 'A') && (c <= 'Z')) ||
 			((c >= 'a') && (c <= 'z'))){
-				if(state == OUT){
-					state = IN;
-					++nw;
-				}
+			putchar(c);
 		}
 		else{
-			state = OUT;
-			if(c == '\n')
-				++nl;
+			putchar('\n');
 		}
 	}
-	printf("No. of lines = %d\n"
-			"No. of words = %d\n"
-			"No. of characters = %d\n"
-			, nl, nw, nc);
 	return 0;
 }
