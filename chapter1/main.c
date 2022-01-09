a590e7018c7e9ab847704406188cc71b0514352c
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-09 12:29:55 +0530

    count lines, words, characters, version 1

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 
-/*count blanks, tabs, newlines in input*/
+#define IN 1	/*inside a word*/
+#define OUT 0	/*inside a word*/
+
+/*count lines, words, and characters in input*/
 int main(int argc, char **argv)
 {
-	int c, nb, nt, nl;
-	nb = 0;
-	nt = 0;
-	nl = 0;
+	int c, nl, nw, nc, state;
 	
+	state = OUT;
+	nl = nw = nc = 0;
 	while((c = getchar()) != EOF){
-		if(c == ' ')
-			++nb;
-		if(c == '\t')
-			++nt;
+		++nc;
 		if(c == '\n')
 			++nl;
+		if(c == ' ' || c == '\t' || c == '\n')
+			state = OUT;
+		else if(state == OUT){
+			state = IN;
+			++nw;
+		}
 	}
-	printf("No. of blanks = %d\n"
-			"No. of tabs = %d\n"
-			"No. of newlines = %d\n"
-			, nb, nt, nl);
+	printf("No. of lines = %d\n"
+			"No. of words = %d\n"
+			"No. of characters = %d\n"
+			, nl, nw, nc);
 	return 0;
 }
