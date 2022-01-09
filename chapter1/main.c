9c1e89d1255454703d4b46d78783e30380ec8cb4
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-09 11:42:17 +0530

    Character input and output using getchar() and putchar()

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,18 +1,12 @@
 #include<stdio.h>
 
-#define LOWER 0 /*lower limit of table*/
-#define UPPER 300 /*upper limit of table*/
-#define STEP 20 /*step size*/
-
-/*
-fahrenheit to celsius converter
-*/
 int main(int argc, char **argv)
 {
-	int fahr;
-	
-	for(fahr = LOWER; fahr <= UPPER; fahr += STEP)
-		printf("%3d\t%6.1f\n",
-				fahr, (5.0 / 9.0) * (fahr - 32));
+	int c;
+	c = getchar();
+	while(c != EOF){		/*EOF -> Unix Ctrl + D, Windows Ctrl + Z*/
+		putchar(c);
+		c = getchar();
+	}
 	return 0;
 }
