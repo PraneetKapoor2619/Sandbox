cdd253dd31fd51654d31c67ae8bd8bf25c506be4
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-09 11:46:27 +0530

    Precedence of != is greater than =. Returns NULL without parenthesis

--- chapter1/main.c
+++ chapter1/main.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
 	int c;
 	
-	while((c = getchar()) != EOF){		/*EOF -> Unix Ctrl + D, Windows Ctrl + Z*/
+	while(c = getchar() != EOF){
 		putchar(c);
 	}
 	return 0;
