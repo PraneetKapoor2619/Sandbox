c9138164022c24248b395d264e2c3a531777ffe4
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-10 10:45:31 +0530

    Histogram of frequencies of different characters in the input

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,42 +1,38 @@
 #include<stdio.h>
 
-#define IN 1				/* inside a word */
-#define OUT 0				/* outside a word */
-#define MAX_WLENGTH	16		/* maximum length of words supported */
-/* print histogram of length of words in the input*/
+#define RANGE 128		/* length of character set supported */
+						/* In this case, ASCII, (128) */
+
+/* print histogram of the frequencies of different characters in the input*/
 int main(int argc, char **argv)
 {
-	int c, state, current_length;
-	int wlength[MAX_WLENGTH];
+	int c;
+	int freq[RANGE];
 	
-	current_length = 0;
-	state = OUT;
-	for(int i = 0; i < MAX_WLENGTH; ++i)
-		wlength[i] = 0;
+	for(int i = 0; i < RANGE; ++i)
+		freq[i] = 0;
 	
-	while((c = getchar()) != EOF){
-		if(((c >= 'A') && (c <= 'Z')) ||
-			((c >= 'a') && (c <= 'z'))){
-			if(state == OUT)
-				state = IN;
-			++current_length;
-		}
-		else{
-			if(state == IN){
-				state = OUT;
-				++wlength[current_length - 1];
-				current_length = 0;
-			}
-		}
-	}
+	while((c = getchar()) != EOF)
+		++freq[c];
 	
 	/* Now print the histogram */
-	for(int i = 0; i < MAX_WLENGTH; ++i){
-		if(wlength[i] > 0){
-			printf("%4d ", i + 1);
-			for(int j = 0; j < wlength[i]; ++j)
+	for(int i = 0; i < RANGE; ++i){
+		if(freq[i] > 0){
+			if(i == 0)
+				printf("NULL");
+			else if(i == '\n')
+				printf("%6s ", "\\n");
+			else if(i == '\t')
+				printf("%6s ", "\\t");
+			else if(i == '\r')
+				printf("%6s ", "\\r");
+			else if(i == ' ')
+				printf("%6s ", "SPACE");
+			else
+				printf("%6c ", i);
+			for(int j = 0; j < freq[i]; ++j)
 				printf("|");
-			printf("%d\n", wlength[i]);
+			printf("%d\n", freq[i]);
 		}
 	}
 	return 0;
