12085ed8373334712eb4b13fd11d48a6dd6e1d90
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-10 18:32:35 +0530

    print longest line in the input. used getlines() because getline() already exists in standard library.

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,23 +1,54 @@
 #include<stdio.h>
 
-int power(int m, int n);
+#define MAXLINE 1000 /* maximum input line size */
 
-/* test power function */
+int getlines(char line[], int maxline);
+void copy(char to[], char from[]);
+
+/* print longest input line */
 int main(int argc, char **argv)
 {
-	int i;
+	int len;		/* current line length */
+	int max;		/* maximum length seen so far */
+	char line[MAXLINE];		/* current input line */
+	char longest[MAXLINE];	/* longest line is saved here */
 	
-	for(int i = 0; i < 10; ++i)
-		printf("%d %d %d\n", i, power(2, i), power(-3, i));
+	max = 0;
+	while((len = getlines(line, MAXLINE)) > 0){
+		if(len > max){
+			max = len;
+			copy(longest, line);
+		}
+	}
+	if(max > 0)		/* there was a line */
+		printf("%s", longest);
 	return 0;
 }
 
-/* power: raise base to nth power; n >= 0; version 2*/
-int power(int base, int n)
+/* getlines: read a line into s, return length */
+int getlines(char s[], int lim)
 {
-	int p;
+	int c, i;
 	
-	for(p = 1; n > 0; --n)
-		p = p * base;
-	return p;
+	for(i = 0; (i < lim - 1) &&
+				((c = getchar()) != EOF) &&
+				(c != '\n');
+				++i)
+		s[i] = c;
+	if(c == '\n'){
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+/* copy: copy 'from' into 'to'; assume it to be big enough */
+void copy(char to[], char from[])
+{
+	int i;
+	
+	i = 0;
+	while((to[i] = from[i])!= '\0')
+		++i;
 }
