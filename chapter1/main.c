671cdc621e8dda43395595729747f1349270c60e
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-10 17:33:11 +0530

    First function. Calculates positive integer power of integers

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,39 +1,24 @@
 #include<stdio.h>
 
-#define RANGE 128		/* length of character set supported */
-						/* In this case, ASCII, (128) */
+int power(int m, int n);
 
-/* print histogram of the frequencies of different characters in the input*/
+/* test power function */
 int main(int argc, char **argv)
 {
-	int c;
-	int freq[RANGE];
+	int i;
 	
-	for(int i = 0; i < RANGE; ++i)
-		freq[i] = 0;
-	
-	while((c = getchar()) != EOF)
-		++freq[c];
-	
-	/* Now print the histogram */
-	for(int i = 0; i < RANGE; ++i){
-		if(freq[i] > 0){
-			if(i == 0)
-				printf("NULL");
-			else if(i == '\n')
-				printf("%6s ", "\\n");
-			else if(i == '\t')
-				printf("%6s ", "\\t");
-			else if(i == '\r')
-				printf("%6s ", "\\r");
-			else if(i == ' ')
-				printf("%6s ", "SPACE");
-			else
-				printf("%6c ", i);
-			for(int j = 0; j < freq[i]; ++j)
-				printf("|");
-			printf("%d\n", freq[i]);
-		}
-	}
+	for(int i = 0; i < 10; ++i)
+		printf("%d %d %d\n", i, power(2, i), power(-3, i));
 	return 0;
+}
+
+/* power: raise base to nth power; n >= 0 */
+int power(int base, int n)
+{
+	int i, p;
+	
+	p = 1;
+	for(i = 1; i <= n; ++i)
+		p = p * base;
+	return p;
 }
