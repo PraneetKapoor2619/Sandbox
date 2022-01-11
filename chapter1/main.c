aca82737e2a594197789eaa484ce951521a4780c
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-11 11:38:17 +0530

    Print all input lines that are longer than 80 characters.

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
 
-#define MAXLENGTH 80		/* Maximum Length of input allowed */
+#define MAXLENGTH 1024		/* Maximum Length of input allowed */
 
 int readline(char [], int);
-void copy(char [], char []);
 
 int main(int argc, char **argv)
 {
 	int l, max;
-	char current_line[MAXLENGTH], longest_line[MAXLENGTH];
+	char line[MAXLENGTH];
 	
-	for(max = 0; (l = readline(current_line, MAXLENGTH)) > 0; ){
-		if(l > max){
-			copy(current_line, longest_line);
-			max = l;
-		}
-	}
-	
-	printf("The longest line is:\n%s\nit has %d characters.\n", longest_line, max);
+	while((l = readline(line, MAXLENGTH)) > 0)
+		if(l > 80)
+			printf("(%d) %s", l, line);
 	return 0;
 }
 
@@ -25,26 +19,13 @@ int readline(char line[], int N)
 {
 	int i, c;
 	
-	for(i = 0; (c = getchar()) != EOF && (c != '\n'); ++i)
-		if(i < N)
-			line[i] = c;
+	for(i = 0; (i < N) && (c = getchar()) != EOF && (c != '\n'); ++i)
+		line[i] = c;
 	
-	if(c == '\n' && i < N){
+	if(c == '\n'){
 		line[i] = c;
 		++i;
-		line[i] = '\0';
 	}
-	else
-		line[N - 1] = '\0';
-
+	line[i] = '\0';
 	return i;
-}
-
-void copy(char from[], char to[])
-{
-	int i;
-	
-	i = 0;
-	while((to[i] = from[i]) != '\0')
-		++i;
 }
