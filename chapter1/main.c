b6016909d525fd16223e0acad7578d491efdb24a
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-13 19:35:26 +0530

    Comment removal program

--- chapter1/main.c
+++ chapter1/main.c
@@ -1,47 +1,35 @@
 #include<stdio.h>
 
 #define MAXLENGTH 1024		/* Maximum Length of input allowed */
-#define MAXCOLUMNS 80		/* maximum number of columns */
-
-void insertnl(char [], int, int, int);
 
 int main(int argc, char **argv)
 {
-	int i, cnt, c, wlength;
+	int c, i, cflag;
 	char line[MAXLENGTH];
 	
-	i = cnt = wlength = 0;
+	i = cflag = 0;
 	while ((c = getchar()) != EOF) {
-		if (c != ' ' && c != '\n')
-			++wlength;
-		else
-			wlength = 0;
-		line[i] = c; 
-		++cnt; 
-		++i;
-		
-		if (cnt == (MAXCOLUMNS + 1)) {
-			insertnl(line, i - wlength, i, '\n');
+		if (c == '/' && cflag == 0) {
+			if ((c = getchar()) == '*')
+				cflag = 1;
+			else {
+				cflag = 0;
+				line[i] = '/';
+				++i;
+				line[i] = c;
+			}
+		}
+		else if (c == '*' && cflag == 1) {
+			if ((c = getchar()) == '/')
+				cflag = 0;
+		}
+		else if (cflag == 0) {
+			line[i] = c;
 			++i;
-			cnt = wlength;
 		}
 	}
 	line[i] = '\0';
 	
-	printf("\n");
-	for (i = 0; i < MAXCOLUMNS; ++i)
-		printf("-");
 	printf("\n%s\n", line);
 	return 0;
-}
-
-void insertnl(char line[], int si, int ei, int tmp)
-{
-	line[ei] = ' ';
-	while (si <= ei) {
-		line[si] = line[si] + tmp;
-		tmp = line[si] - tmp;
-		line[si] = line[si] - tmp;
-		++si;
-	}
 }
