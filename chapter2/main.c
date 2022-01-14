52e46dddc62aea3b5b98a0e0dcce3f5fcb66a9fe
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-01-14 13:45:35 +0530

    Printing size of data types using overflow. Size of float could not be determined. First use of GDB

--- chapter2/main.c
+++ chapter2/main.c
@@ -1,32 +1,47 @@
 #include<stdio.h>
-#include<limits.h>
-#include<float.h>
 
 int main(int argc, char **argv)
 {
-	printf("\nRange of char"
-			"\nSigned char: %d to %d"
-			"\nUnsigned char: %d to %d\n",
-			SCHAR_MIN, SCHAR_MAX, 0, UCHAR_MAX);
-			
-	printf("\nRange of int"
-			"\nSigned int: %d to %d"
-			"\nUnsigned int: %d to %ld\n",
-			INT_MIN, INT_MAX, 0, UINT_MAX);
-			
-	printf("\nRange of short"
-			"\nSigned short: %d to %d"
-			"\nUnsigned short: %d to %d\n",
-			SHRT_MIN, SHRT_MAX, 0, USHRT_MAX);
+	char c;
+	int i;
+	short int si;
+	long int li;
+	float f;
 	
-	printf("\nRange of long"
-			"\nSigned long: %ld to %ld"
-			"\nUnsigned long: %ld to %lu\n",
-			LONG_MIN, LONG_MAX, 0, ULONG_MAX);
+	int nc, ni, nsi, nli, nf;
 	
-	printf("\nRange of float"
-			"\nFloat: %f to %f\n",
-			FLT_MIN, FLT_MAX);
+	c = i = si = li = f = 2;
+	nc = ni = nsi = nli = nf = 1;
+	
+	while (c != 0 || i != 0 || si != 0 || li != 0 || f != 0) {
+		if (c != 0) {
+			c = c * 2;
+			++nc;
+		}
+		if (i != 0) {
+			i = i * 2;
+			++ni;
+		}
+		if (si != 0) {
+			si = si * 2;
+			++nsi;
+		}
+		if (li != 0) {
+			li = li * 2;
+			++nli;
+		}/*
+		if (f != 0) {
+			f = f * 2.0;
+			++nf;
+		}*/
+		
+	}
+	
+	printf("\nSize of char: %d", nc / 8);
+	printf("\nSize of int: %d", ni / 8);
+	printf("\nSize of short int: %d", nsi / 8);
+	printf("\nSize of longt int: %d", nli / 8);
+	//printf("\nSize of float: %d", nf / 8);
 	
 	return 0;
 }
