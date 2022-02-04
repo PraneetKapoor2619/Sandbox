26dbf8715f83c12dc4b1bafa0079786621fe0a6c
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-02-04 18:22:21 +0530

    printf now supports LF, doesn't work properly tho!

--- hb-os/main.s
+++ hb-os/main.s
@@ -1,10 +1,15 @@
 section .data
 	vga_start equ 0x0b8000
 	vga_end equ 0x0c0000
+	vga_col equ 0x50
 	vga_ptr dd 0x0b8000
 	;string and its length
-	string db "Hello, world!!"
-	len equ $ - string
+	string1 db "Hello world!!", 0x0a
+	len1 equ $ - string1
+	string2 db "This is P.K.'s Personal Operating System!!", 0x0a, 0x0a
+	len2 equ $ - string2
+	string3 db "Welcome, children!!", 0x0a
+	len3 equ $ - string3
 
 section .text
 bits 32
@@ -13,6 +18,14 @@ start:
 	; print `OK`
 	call clear_screen
 	;print the string
+	mov eax, len1
+	mov ecx, string1
+	call printf
+	mov eax, len2
+	mov ecx, string2
+	call printf
+	mov eax, len3
+	mov ecx, string3
 	call printf
 	hlt
 
@@ -21,32 +34,53 @@ start:
 clear_screen:
 	mov dword [vga_ptr], vga_start
 	mov ebx, dword [vga_ptr]
-clrs_1:
+.1:
 	;operation start
 	mov word [ebx], 0x0000
 	;operation end
 	add ebx, 0x02
 	cmp ebx, vga_end
-	jb clrs_1
+	jb .1
 	ret
 
 ;SUB: write_screen
 ;PURPOSE: write a string to the screen
 printf:
-	mov eax, len
-	mov ecx, 0x00
+	add eax, ecx
 	mov ebx, dword [vga_ptr]
-printf_1:
-	;operation start
-	mov dl, byte [string + ecx]
+.1:	;operation start
+	cmp byte [ecx], 0x0a
+	je .3
+	mov dl, byte [ecx]
 	mov dh, 0x0f
 	mov word [ebx], dx
-	add ebx, 0x02
 	;operation end
+.2	add ebx, 0x02
 	inc ecx
 	cmp eax, ecx
-	jne printf_1
-	je printf_2
-printf_2:
+	jne .1
 	mov dword [vga_ptr], ebx
 	ret
+	
+.3:
+	mov dl, 0x00
+	mov dh, 0x0f
+	mov word [ebx], dx
+	push eax
+	mov eax, ebx
+	sub eax, vga_start		;total columns = vga_ptr - vga_start
+	push ebx
+	mov edx, 0x00
+	mov ebx, 0x02			;because 1 column = (character + bgfg)
+	div ebx				;eax = total columns
+	mov edx, 0x00
+	mov ebx, vga_col
+	div ebx				;divide total col by no. of col
+	pop ebx
+	pop eax
+	cmp edx, 0x00			;if total col % 0x50, then edx = 0x00
+	je .4				;first fix the ptr
+	add ebx, 0x02
+	jmp .3				;check again
+.4:	sub ebx, 0x02
+	jmp .2				;now return to print the rest of string
