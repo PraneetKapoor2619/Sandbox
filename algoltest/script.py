fb8c4ea9aaad189c88de36190ae8f148b780acc1
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-05-12 13:14:46 +0530

    Program for translating LBA to CHS

--- algoltest/script.py
+++ algoltest/script.py
@@ -1,27 +1,7 @@
-GETLINE_BUFFER = input()
-num = 80 - len(GETLINE_BUFFER)
-while (num):
-        GETLINE_BUFFER += '\0'
-        num -= 1
-flag = 0
-count = 0
-words = []
-for i in range(0, 80):
-        words.append(0)
-print(len(words))
-di = 0
-for si in range(0, 80):
-        if (GETLINE_BUFFER[si] == '\0'):
-                break
-        if (GETLINE_BUFFER[si] == ' '):
-                flag = 0
-                continue
-        else:
-                if (flag == 0):
-                        flag = 1
-                        di = 16 * count
-                        count += 1
-                words[di] = GETLINE_BUFFER[si]
-                di += 1
-for di in range (0, 80):
-        print(di, ":", words[di])
+LBA = 0
+while (True):
+        LBA = int(input())
+        C = int(LBA / (2 * 18))
+        H = int((LBA / 18) % 2)
+        S = int((LBA % 18) + 1)
+        print("C:", C, "H:", H, "S:", S)
