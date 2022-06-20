7b070298427f22781c53a86c0dcb6001fa02d911
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-06-20 13:22:34 +0530

    Method #2 of mutating a string

--- hcl_training/week3/mutate_string.py
+++ hcl_training/week3/mutate_string.py
@@ -1,7 +1,5 @@
 def mutate_string(string, position, character):
-        str_arr = list(string)
-        str_arr[position] = character
-        return "".join(str_arr)
+        return string[:position] + character + string[position + 1:]
 
 if __name__ == '__main__':
         s = input()
