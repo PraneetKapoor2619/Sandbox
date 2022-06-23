c97f5e91cb5893995feff7d7c546403a54e3c68a
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-06-23 20:50:45 +0530

    A simpler one I found on the internet

--- hcl_training/week3/the-minion-game.py
+++ hcl_training/week3/the-minion-game.py
@@ -1,35 +1,19 @@
-def find_substring(string, set_of_substrings):
-        count = 0 
-        for substring in set_of_substrings :
-                index = 0
-                while True :
-                        index = string.find(substring, index) + 1
-                        if (index > 0) :
-                                count += 1
-                        else :
-                                break
-        return count
-
 def minion_game(string):
-        # use a set so that the slices don't get repeated again and again
-        set_of_substrings = {1}
-        set_of_substrings.clear()
-
-        for i in range(len(string)) :
-                for j in range(len(string) - i) :
-                        set_of_substrings.update([string[j:j + i + 1]])
-        
-        kevin = set(filter(lambda x: x[0] in ['A', 'E', 'I', 'O', 'U'], set_of_substrings))
-        stuart = set_of_substrings.difference(kevin)
-        
-        score_kevin, score_stuart = find_substring(string, kevin), find_substring(string, stuart)
-        
-        if score_kevin > score_stuart :
-                print("Kevin", score_kevin)
-        elif score_stuart > score_kevin :
-                print("Stuart", score_stuart)
-        else :
-                print("Draw")
+        vowels = 'AEIOU'
+        Stuart_score, Kevin_score = 0, 0
+        length = len(string)
+        for start_idx in range(length):
+                score = length - start_idx
+                if string[start_idx] in vowels:
+                        Kevin_score += score
+                else:
+                        Stuart_score += score
+        if Stuart_score == Kevin_score:
+                print('Draw')
+        if Stuart_score > Kevin_score:
+                print('Stuart {}'.format(Stuart_score))
+        if Stuart_score < Kevin_score:
+                print('Kevin {}'.format(Kevin_score))
 
 
 if __name__ == '__main__':
