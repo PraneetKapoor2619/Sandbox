e5314a63d03436be11569f1081f32312665991ff
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-06-23 09:39:29 +0530

    Shallow and Deep copies

--- hcl_training/week3/copies.py
+++ hcl_training/week3/copies.py
@@ -1,10 +1,30 @@
-def func1(arr_p):
-        print(id(arr_p))
-        arr_p[0] = 12
+import copy
 
-arr = [1, 2, 3, 4]
-arr_1 = arr
-print(id(arr))
-print(id(arr_1))
-func1(arr)
-print(arr, arr_1)
+def shallow_copy1(arr_p):
+        print(id(arr_p), id(arr_p[0]))
+        arr_p[0] = [10, 20, 30]
+        print(id(arr_p[0]))
+
+def shallow_copy2(arr_p):
+        print(id(arr_p), id(arr_p[0]))
+        arr_p[0][1] = 455
+
+def deep_copy1(arr_p):
+        print(id(arr_p), id(arr_p[0]))
+        arr_p[0][1] = 2
+
+
+def deep_copy2(arr_p):
+        print(id(arr_p), id(arr_p[0]))
+        arr_p[0][1] = 2
+
+arr = [[1, 2, 3], [4, 5, 6]]
+print(id(arr), id(arr[0]))
+shallow_copy1(copy.copy(arr))
+print(arr)
+shallow_copy2(copy.copy(arr))
+print(arr)
+deep_copy1(copy.deepcopy(arr))
+print(arr)
+deep_copy2([x[:] for x in arr])
+print(arr)
