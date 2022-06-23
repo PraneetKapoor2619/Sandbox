bbcaa701f5a9630cf6d38e073ea546ddfd4bdf46
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-06-23 14:16:19 +0530

    This one doesn't work for 1 w 2 r 3g, gives 1 W 2 R 3G

--- hcl_training/week3/capitalize.py
+++ hcl_training/week3/capitalize.py
@@ -8,12 +8,7 @@ import sys
 
 # Complete the solve function below.
 def solve(s):
-        splits = s.split()
-        splits = [string.capitalize() for string in splits]
-        result = " ".join(splits)
-        return result
-
-
+        return s.title()
 
 if __name__ == '__main__':
         s = input()
