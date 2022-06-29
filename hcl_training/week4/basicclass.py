75b7f14d9ee27d45ff8c150cba71820a814e523a
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-06-29 09:20:59 +0530

    Basic classes in Python

---
+++ hcl_training/week4/basicclass.py
@@ -0,0 +1,13 @@
+class temp1:
+        x = int()
+        y = float()
+
+class temp2:
+        pass
+ 
+obj1 = temp1()
+obj2 = temp2()
+obj1.x = 1
+obj1.y = 3.14159
+print(obj1.x, obj1.y)
+print(obj2)
