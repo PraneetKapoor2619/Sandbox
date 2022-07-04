fa4a06e0565f3d3caf371921aa2b70066398c2a8
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-07-04 09:06:43 +0530

    Cleanup

--- binfiletest/asm.asm
+++
@@ -1,7 +0,0 @@
-bits 16
-align 16
-mov bx, 0xb800
-mov es, bx
-xor bx, bx
-mov word [es:bx], 0x0c58
-retf
--- nfs_scripts/min_max_comp.m
+++
@@ -1,5 +0,0 @@
-R = input("Input the first vector: ");
-S = input("Input the second vector: ");
-
-[i, j] = size(R);
-[m, n] = size(S);
