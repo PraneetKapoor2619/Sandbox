fd49a13c11da512f50262e2dfacb9e631cdf74bb
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-07-16 11:39:27 +0530

    Yes babygit add . Hogayagit add .

--- hcl_training/week6/regex-substitution.py
+++ hcl_training/week6/regex-substitution.py
@@ -1,8 +1,8 @@
 import re
 
 def replace(match):
-        if match.group(0) == " && " : return " and "
-        else : return " or "
+        if match.group(0) == r"&&" : return "and"
+        elif match.group(0) == "||": return "or"
 
 if __name__ == "__main__" :
         buffer = ""
@@ -10,8 +10,7 @@ if __name__ == "__main__" :
         for _ in range(int(input())) :
                 buffer += input() + "\n"
 
-        str_and = r" && "
-        str_or = r" \|\| "
-        buffer = re.sub(r"(%s|%s)" % (str_and, str_or), replace, buffer)
-        print()
+        str_and = r"&&"
+        str_or = r"\|\|"
+        buffer = re.sub(r"(?<=\s)(%s|%s)(?=\s)" % (str_and, str_or), replace, buffer)
         print(buffer)
