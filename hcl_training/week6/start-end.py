2e8296487c2e8475a7db6d1d0a562a53bdfa78cb
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-07-16 10:36:35 +0530

    Bad solution

--- hcl_training/week6/start-end.py
+++ hcl_training/week6/start-end.py
@@ -2,5 +2,13 @@ import re
 
 S = input()
 k = input()
-m = re.findall(r'(?<=a)(%s)' % (k), S)
-print(m)
+
+match_list = re.finditer(r"(?=(%s))" % (k), S)
+
+flag = False
+
+for match in match_list :
+        flag = True
+        print((match.start(1), match.end(1) - 1))
+
+if flag == False : print((-1, -1))
