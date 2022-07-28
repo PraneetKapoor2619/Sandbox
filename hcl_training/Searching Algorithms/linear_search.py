57998ed659abd0936fff5c1e15aa65ea812c2ec1
Praneet Kapoor <kapoorpraneet2619@gmail.com>
2022-07-28 18:23:41 +0530

    Program to perform linear search on an array

---
+++ hcl_training/Searching Algorithms/linear_search.py
@@ -0,0 +1,22 @@
+import random
+import time
+
+
+if __name__ == "__main__" :
+	array = sorted([random.randint(0, 100) for i in range(100)])
+
+	time_array = []
+	for _ in range(1000) :
+		element = random.randint(0, 100)
+		index = None
+		
+		start_time = time.time()
+		for i in range(len(array)):
+			if (array[i] == element) :
+				index = i
+				break
+		end_time = time.time()
+		time_array.append(end_time - start_time)
+
+	print("Average time required in finding an element:",\
+	sum(time_array) / len(time_array))
